530b1ec13a408211ea3423946f5511f14aa07b5d
MatthewH777 <MatthewHering7@gmail.com>
2025-11-25 17:20:04 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 async function getData(major) {
-    var response = await fetch("cit5students.json"); // get request
+    const response = await fetch("cit5students.json");
+    const data = await response.json();
 
-    if(response.ok) {
-        var data = await response.json();
+    // Filter dataset
+    const filtered = data.filter(student => student.Major === major);
 
-    // filter data array
-    let filterMajor = data.filter(name => name.major == major );
+    // Compile template
+    const templateText = document.getElementById('menuTemplate').innerHTML;
+    const compiledTemplate = Handlebars.compile(templateText);
+    const compiledHtml = compiledTemplate({ rows: filtered });
 
-    var templateText = document.getElementById('menuTemplate').innerHTML;
-    var compiledTemplateText = Handlebars.compile(templateText);
-    var compiledHtml = compiledTemplateText({ rows: filterMajor}) // add filter using what we made on line 5
-
-    let table = document.getElementById('dataTable');
-    table.style.display = "table";
-    table.innerHTML = compiledHtml;
-
-    }
-    else {
-        document.querySelector('#dataTable').innerHTML = "There was an error."
-    }
+    // Insert into table
+    document.getElementById('dataTable').innerHTML = compiledHtml;
 }
-
