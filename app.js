0684f5e9b0bcd7d060ce3080af866de07e61346b
MatthewH777 <MatthewHering7@gmail.com>
2025-11-25 17:33:41 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ async function getData(major) {
     const data = await response.json();
 
     // Filter dataset
-    const filtered = data.filter(student => student.Major === major);
+    const filtered = data.filter(student => student.major === major);
 
     // Compile template
     const templateText = document.getElementById('menuTemplate').innerHTML;
     const compiledTemplate = Handlebars.compile(templateText);
     const compiledHtml = compiledTemplate({ rows: filtered });
 
+    // Insert into table container
     const table = document.getElementById('dataTable');
     table.innerHTML = compiledHtml;
-    table.style.display = 'table'; // make it visible
-
-
-    // Insert into table
-    document.getElementById('dataTable').innerHTML = compiledHtml;
+    table.style.display = 'block'; // display
 }
-
