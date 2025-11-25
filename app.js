f6ff69da7280ecf826662cc9a660c680c8695e4b
MatthewH777 <MatthewHering7@gmail.com>
2025-11-25 16:49:18 -0600

    update files

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-async function getData(Major) {
+async function getData(major) {
     var response = await fetch("cit5students.json"); // get request
 
+    if(response.ok) {
+        var data = await response.json();
+
     // filter data array
-    Major = data.filter( (major) => HTMLTableRowElement.Major == major );
+    filterMajor = data.filter(name => name.major == major );
 
     var templateText = document.getElementById('menuTemplate').innerHTML;
     var compiledTemplateText = Handlebars.compile(templateText);
-    compiledHtml = compiledTemplateText({ rows: Major}) // add filter using what we made on line 5
-    document.getElementById('dataTable')
+    compiledHtml = compiledTemplateText({ rows: filterMajor}) // add filter using what we made on line 5
+    document.getElementById('dataTable').innerHTML = compiledHtml;
+    }
+    else {
+        document.querySelector('#dataTable').innerHTML = "There was an error."
+    }
 }
